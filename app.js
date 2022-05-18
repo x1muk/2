fe3ea95efcfca321acfabe24a3ae669623e85285
Andrew Dji <xumxga@gmail.com>
2022-05-19 00:40:25 +0300

    fix title filter

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function init() {
         // Теперь создадим список, содержащий 5 пунктов.
         listBoxControl = new ymaps.control.ListBox({
             data: {
-                content: 'Фильтр',
+                content: 'Жмакай туть',
                 title: 'Фильтр'
             },
             items: listBoxItems,
